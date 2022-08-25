799e871c47b6e6084f051ee98e0accb626d0550b
杨伟泽 <1179132719@qq.com>
2022-08-25 20:10:23 +0800

    添加Tailwind CSS

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router/index"
+import './index.css'
+
 createApp(App).use(router).mount('#app')
