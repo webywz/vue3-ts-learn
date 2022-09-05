5aa91d451908c6a2809d535216c9056b5de654e1
杨伟泽 <1179132719@qq.com>
2022-09-05 15:54:15 +0800

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,14 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:vue/vue3-essential', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
+    'vue/multi-word-component-names': 0
   }
 }
